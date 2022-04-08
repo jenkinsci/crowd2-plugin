f2e933f2a4c17cfb4844ab220bbc51678d76e59b
dumam <9051964+DuMaM@users.noreply.github>
2022-04-08 14:13:55 +0000

    Add setup as a code for crowd

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export GITPOD_SITE="${HOSTNAME}.${GITPOD_WORKSPACE_CLUSTER_HOST}"
+export JENKINS_SITE_NAME="8080-${GITPOD_SITE}"
+export CROWD_SITE_NAME="8095-${GITPOD_SITE}"
+
+export JENKINS_URL="https://$JENKINS_SITE_NAME/"
+export CROWD_BACKUP_FILE="./casc/crowd_backup.xml"
+
+# create new img 
+if [ $# -gt 0 ]; then
+    docker-compose -f casc/docker-compose.yml build
+fi
+
+# start docker services
+docker-compose -f casc/docker-compose.yml up -d
+
+# replace crowd address to current one
+sed -i "s/http:\/\/.*\/crowd/http:\/\/$CROWD_SITE_NAME\/crowd/" "$CROWD_BACKUP_FILE"
+# replace jenkins address to current one
+sed -i "s/https:\/\/\(\w\|\-\|\.\)\+\//https:\/\/$JENKINS_SITE_NAME/" "$CROWD_BACKUP_FILE"
+# add crowd backup to instance
+docker cp "$CROWD_BACKUP_FILE" crowd:"crowd_backup.xml"
+
+echo "Now please setup database according to your preferences"
