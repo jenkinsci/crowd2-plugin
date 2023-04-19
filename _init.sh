d0da91b626185e921f4c26da73b7086996198878
Bartosz Nowak <9051964+DuMaM@users.noreply.github.com>
2023-04-19 21:46:24 +0200

    fix: script now is correctly interpreting input variables

--- _init.sh
+++ _init.sh
@@ -10,12 +10,12 @@ if [[ -f "${MVN_SETTINGS_PATH}" ]]; then
     mv "${MVN_SETTINGS_PATH}" "${MVN_SETTINGS_PATH}.bak${BACKUP_DATE}"
 fi
 
-if [[ -n "${ARTIFACTORY_PASS}" ]]; then
+if [[ -z "${ARTIFACTORY_PASS}" ]]; then
     echo "Please define variable ARTIFACTORY_PASS"
     exit 1
 fi
 
-if [[ -n "${ARTIFACTORY_USER}" ]]; then
+if [[ -z "${ARTIFACTORY_USER}" ]]; then
     echo "Please define variable ARTIFACTORY_USER"
     exit 1
 fi
