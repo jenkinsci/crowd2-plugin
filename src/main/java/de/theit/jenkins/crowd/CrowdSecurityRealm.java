3d9bb22d41aa45e033aa25be026e086ec593d7b1
Bartosz Nowak <9051964+DuMaM@users.noreply.github.com>
2023-05-08 01:24:23 +0200

    fix: descriptor

--- src/main/java/de/theit/jenkins/crowd/CrowdSecurityRealm.java
+++ src/main/java/de/theit/jenkins/crowd/CrowdSecurityRealm.java
@@ -547,7 +547,7 @@ public class CrowdSecurityRealm extends AbstractPasswordBasedSecurityRealm {
      * @version $Id$
      */
     @Extension
-    public static final class DescriptorImpl extends Descriptor<CrowdSecurityRealm> {
+    public static final class DescriptorImpl extends Descriptor<SecurityRealm> {
         /**
          * Default constructor.
          */
